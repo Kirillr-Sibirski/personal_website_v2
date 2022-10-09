cedca9321bbe84a94c2a5a44a32a35aabd80c6f9
Kirillr-Sibirski <kirill.rybkov@outlook.com>
2022-10-09 13:42:22 +0200

    Projects page, redirect on scroll

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
-import { DiGithubBadge, AiOutlineMail, BsTwitter, BsLinkedin } from "react-icons";
+import { FaGithub, FaMailBulk, FaTwitter, FaLinkedin } from "react-icons/fa";
 import './about.css';
+import { useNavigate } from 'react-router-dom';
 
 export const About = () => {
+  document.body.style = 'background: #262322;';
+  window.scrollTo(0, 0)
+  const navigate = useNavigate();
+
+  function handleScroll() { 
+    
+    var isAtBottom = document.documentElement.scrollHeight - document.documentElement.scrollTop <= document.documentElement.clientHeight; 
+    
+    if (isAtBottom) { 
+      navigate('/projects');
+    } 
+    
+  } 
+  
+  window.addEventListener("scroll", handleScroll);
+
   return (
-    <div>
-      <div className="title">Kirill Rybkov</div>
-      <div className="text">
-          <span className="font-link">
-          <p>&nbsp; Born in Siberian part of Russia. I'm living & studying in Barcelona, Spain. Capitalist and atheist, though I do believe in the <b>Universe.</b> I'm a geek (at least how I classify myself) with soldering iron in teeth, having ~2 years coding experience in hands and the same amount of self-development in conscious and subconscious minds. A lot of creativity and imagination is added and well mixed. Love dreaming about the future of the humankind and space. I believe my mission is to hugely advance and expand the human race, as well as make a revolution.</p>
-          <p>&nbsp; Now working in web3 & blockchain space after working in game dev for a year. Built a lot of cool stuff during hackathons and bounty hunting marathons. I do believe that blockchain is the future of more sustainable capitalism without corruption, crimes and stupid policies that will one day help the humans to expand in supercalifragilisticexpialidocious way.</p>
-          <p>&nbsp; I prefer working as a back-end developer, however I can do front-end so I guess I'm full-stack.</p>
-          </span>
+      <div id="page1">
+          <div className="title">Kirill Rybkov</div>
+          <div className="text">
+              <span className="font-link">
+              <p>&nbsp; Born in Siberian part of Russia. I'm living & studying in Barcelona, Spain. Capitalist and atheist, though I do believe in the <b>Universe.</b> I'm a geek (at least how I classify myself) with soldering iron in teeth, having ~2 years coding experience in hands and the same amount of self-development in conscious and subconscious minds. A lot of creativity and imagination is added and well mixed. Love dreaming about the future of the humankind and space. I believe my mission is to hugely advance and expand the human race, as well as make a revolution.</p>
+              <p>&nbsp; Now working in web3 & blockchain space after working in game dev for a year. Built a lot of cool stuff during hackathons and bounty hunting marathons. I do believe that blockchain is the future of more sustainable capitalism without corruption, crimes and stupid policies that will one day help the humans to expand in supercalifragilisticexpialidocious way.</p>
+              <p>&nbsp; I prefer working as a back-end developer, however I can do front-end so I guess I'm full-stack.</p>
+              </span>
+          </div>
+            <div className='icons'>
+              < FaGithub />
+              < FaMailBulk />
+              < FaTwitter />
+              < FaLinkedin />
+            </div>
+          <div className='image'></div>
       </div>
-      <div className='icons'>< DiGithubBadge /><AiOutlineMail /><BsTwitter /><BsLinkedin /></div>
-      <div className='image'></div>
-    </div>
   )
 }
 
--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -2,8 +2,27 @@ import React from 'react';
 import './projects.css';
 
 export const Projects = () => {
+  document.body.style = 'background: #F2E5D7;';
+  window.scrollTo(0, 0)
   return (
-    <div>Projects</div>
+    <div id="page2">
+    <div className="title2">Projects</div>
+    <div className="text2">
+        <span className="font-link2">
+        <p>&nbsp; Wood System using RAGE:MP API A system for sale. Uses JS on front-end and C# on server-side. Built while working in GTA5 game dev. https://ragemp.pro/threads/prodazha-v2-sistema-rubki-dereva-dlja-redage-1-1-wood-system.3292/</p>
+        <p>&nbsp; The Loan Stream A DeFi protocol which allows DAOs with large treasuries & other DeFi users to collateralize their treasuries, and take out loans in the form of streams. Built during ‘The Road to Web3’ hackathon. https://showcase.ethglobal.com/roadtoweb3/the-loan-stream</p>
+        <p>&nbsp; With NewEnvAI people can freely mint dynamic animal NFTs which represent real environmental data. The uniqueness of this project is determined by the usage of DALL-E neural network which allows users to create any images by just writing a description of it (e.g. bear in river)! There are 3 stages of NFTs: 'bad', 'neutral' and 'good' that will change accordingly to the data provided by an oracle from environmental API. Built during ‘Sustainable Blockchain Hackathon 2022’. https://github.com/Kirillr-Sibirski/newEnvAI</p>
+        <p>&nbsp; A productivity dapp that will help users to have more conscious life. Increase concentration during work times or help relax more effectively by incentivizing users using awards such as completion NFTs and reward in native tokens. Built during ‘ETHOnline 2022’ hackathon. https://ethglobal.com/showcase/onmagellanic-vvk4q</p>
+        <p>I’ve built a lot more projects but I think these are the most successful. More projects coming!</p>
+        </span>
+    </div>
+    <div className='images2'>
+      <div className='gta'></div>
+      <div className='thels'></div>
+      <div className='newenvai'></div>
+      <div className='onmagellanic'></div>
+    </div>
+</div>
   )
 }
 
