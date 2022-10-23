fdbe3ce94d4a4218bbc5519babef9c3da2d8e919
Kirillr-Sibirski <kirill.rybkov@outlook.com>
2022-10-23 19:51:02 +0200

    change about text

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -14,9 +14,9 @@ export const Main = () => {
          <div className="title">Kirill Rybkov</div>
           <div className="text">
               <span className="font-link">
-              <p>&nbsp; Born in Siberian part of Russia. I'm living & studying in Barcelona, Spain. Capitalist and atheist, though I do believe in the <b>Universe.</b> I'm a geek (at least how I classify myself) with soldering iron in teeth, having ~2 years coding experience in hands and the same amount of self-development in conscious and subconscious minds. A lot of creativity and imagination is added and well mixed. Love dreaming about the future of the humankind and space. I believe my mission is to hugely advance and expand the human race, as well as make a revolution.</p>
-              <p>&nbsp; Now working in web3 & blockchain space after working in game dev for a year. Built a lot of cool stuff during hackathons and bounty hunting marathons. I do believe that blockchain is the future of more sustainable capitalism without corruption, crimes and stupid policies that will one day help the humans to expand in supercalifragilisticexpialidocious way.</p>
-              <p>&nbsp; I prefer working as a back-end developer, however I can do front-end so I guess I'm full-stack.</p>
+              <p>&nbsp; Born in Siberian part of Russia. I’m living & studying in Barcelona, Spain. I’m a geek with soldering iron in teeth, having ~2 years coding experience in hands and the same amount of self-development in mind. A lot of creativity and imagination is added and well mixed. Love dreaming about the future of the humankind and space. I believe my mission is to hugely advance and expand the human race, as well as make a revolution.</p>
+              <p>&nbsp; Now working in web3 & blockchain space after working in game dev for a year. Built a lot of cool stuff during hackathons. I do believe that blockchain is the future of more sustainable capitalism without corruption, crimes and stupid policies that will one day help the humans to hugely expand.</p>
+              <p>&nbsp; I prefer working as a back-end developer, however I can do front-end.</p>
               </span>
           </div> 
           <div className='icons'>
